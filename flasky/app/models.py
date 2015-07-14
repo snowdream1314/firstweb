da46bd47032a04d06af3dcd3d091e96366128ff4
snowdream1314 <595968602@qq.com>
2015-07-14 16:19:23 +0800

    add post articles

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -43,6 +43,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -57,6 +65,7 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -65,7 +74,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(permissions=0xff).first() 
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
-        if self.email is not None and self .avatar_hash is None:
+        if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
     
     @property
@@ -162,6 +171,7 @@ class AnonymousUser(AnonymousUserMixin):
     
     def is_administrator(self):
         return False
+    
 
 login_manager.anonymous_user = AnonymousUser
 
