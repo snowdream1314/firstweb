b3d8a1b428db4a9314f995668ae8b5951f0d10d8
snowdream1314 <595968602@qq.com>
2015-07-15 08:18:25 +0800

    add _posts and edit_post at 11end

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -5,6 +5,8 @@ from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 import hashlib
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -201,6 +203,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
     
     @staticmethod
     def generate_fake(count=100):
@@ -215,3 +218,10 @@ class Post(db.Model):
                     timestamp=forgery_py.date.date(True), author=u)
             db.session.add(p)
             db.session.commit()
+            
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a','abbr','acronym','b','blockquote','code','em',
+                        'i','li','ol','pre','strong','ul','h1','h2','h3','p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'), tags=allowed_tags, strip=True))
+db.event.listen(Post.body, 'set', Post.on_changed_body)
