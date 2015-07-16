77d07db2b4bd3a53869ef049e5ea38f50736259f
snowdream1314 <595968602@qq.com>
2015-07-16 08:41:21 +0800

    add comments at 13end

--- flasky/app/main/forms.py
+++ flasky/app/main/forms.py
@@ -46,3 +46,8 @@ class EditProfileAdminForm(Form):
 class PostForm(Form):
     body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class CommentForm(Form):
+    body = StringField('', validators=[Required()])
+    submit = SubmitField('Submit')
